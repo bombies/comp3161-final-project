4169d3089766049a31e357bf2659cf91c0905aae
bombies <xjamesdzn@gmail.com>
2024-05-07 21:38:30 -0500

    Update port parsing

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ import os
 
 # If file is called directly called, then run the app on the PORT provided defined in ENV or use '3000'.
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=os.getenv("PORT", 3000), debug=bool(os.getenv("DEBUG", False)))
+    app.run(
+        "0.0.0.0",
+        port=int(os.getenv("PORT", 3000)),
+        debug=bool(os.getenv("DEBUG", False)),
+    )
